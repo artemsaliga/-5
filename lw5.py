d0b810a038f95bdc8f1c5b21dcc19a4595a13c2c
artemsaliga <145282713+artemsaliga@users.noreply.github.com>
2023-11-20 14:40:04 +0200

    Add files via upload

---
+++ lw5.py
@@ -0,0 +1,27 @@
+#Саліга Артем КІ1
+#ЛР№5 Аналіз тексту
+import re
+from collections import Counter
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
+
+def get_word_frequency(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    word_count = Counter(words)
+    return word_count
+
+def main():
+    file_path = 'Lw5.txt'
+    text = read_file(file_path)
+    word_frequency = get_word_frequency(text)
+
+    print("Слово\t\tПоширення")
+    print("------------------------")
+    for word, count in word_frequency.items():
+        print(f"{word.ljust(15)}{count}")
+
+if __name__ == "__main__":
+    main()
